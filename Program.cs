dd17fd45e0f9120858e1548673b9caaaad465e2e
Zeritfreeman <47009881+Zeritfreeman@users.noreply.github.com>
2019-01-25 11:28:41 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el angulo D ");
+            double d = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el angulo B ");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la longitud del lado y ");
+            double y = double.Parse(Console.ReadLine());
+
+            double e = 180.0 - (d + b);
+            double c = 180.0 - e;
+            double z = Math.Tan(c * (Math.PI / 180.0)) * y;
+
+            Console.WriteLine("La longitud del lado z es " + z);
+        }
+    }
+}
