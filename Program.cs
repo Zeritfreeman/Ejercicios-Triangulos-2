f948c7d30649fdb94be46b3b2fe7b65790284533
Zeritfreeman <47009881+Zeritfreeman@users.noreply.github.com>
2019-01-25 11:29:49 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la longitud del lado w ");
+            double w = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la longitud del lado t ");
+            double t = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el angulo C ");
+            double c = double.Parse(Console.ReadLine());
+
+            double z = Math.Sin(c * (Math.PI / 180.0)) * t;
+            double y = Math.Cos(c * (Math.PI / 180.0)) * t;
+            double xy = Math.Sqrt((w * w) - (z * z));
+            double x = xy - y;
+
+            Console.WriteLine("La longitud del lado x es " + x);
+        }
+    }
+}
